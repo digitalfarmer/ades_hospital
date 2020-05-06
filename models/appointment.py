d353155a1332ead99f48e055d0bd624e19ac34df
Ades <ades@binasanprima.com>
2020-05-06 09:30:14 +0700

    add related field in odoo and create class appointments and adding age and name related to patient class

--- models/appointment.py
+++ models/appointment.py
@@ -9,12 +9,13 @@ class HospitalAppointment(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
-            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
+            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment.sequence') or _('New')
         result= super(HospitalAppointment, self).create(vals)
         return result
 
     name= fields.Char('Appointment ID', required=True, copy=False, readOnly=True, index=True, default=lambda self: _('New'))
-    patient_id= fields.Many2one('hospital.patient', string='Patient', required=True)
+    patient_id= fields.Many2one('hospital.patient', string='Patient ID', required=True)
+    patient_name= fields.Char('Patient Name',related='patient_id.patient_name')
     patient_age= fields.Integer('Age' , related='patient_id.patient_age')
     notes= fields.Text('Registration Note')
     appointment_date= fields.Date('Date', required=True)
