40317ea57f41f51c5dd6cc8ce3e333ccd6b31049
Ade Setiawan <adesbsp@gmail.com>
2020-06-19 08:38:50 +0700

    saving context from wizard

--- wizards/create_appointment.py
+++ wizards/create_appointment.py
@@ -5,3 +5,21 @@ class CreateAppointment(models.TransientModel):
 
     patient_id= fields.Many2one('hospital.patient',string='Patient')
     appointment_date=fields.Date()
+
+
+    def create_appointment(self):
+        vals = {
+            'patient_id': self.patient_id.id,
+            'appointment_date': self.appointment_date,
+            'notes': 'Created From The Wizard/Code'
+        }
+        new_appointment = self.env['hospital.appointment'].create(vals)
+        context = dict(self.env.context)
+        context['form_view_initial_mode'] = 'edit'
+        return {'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'hospital.appointment',
+                'res_id': new_appointment.id,
+                'context': context
+                }
